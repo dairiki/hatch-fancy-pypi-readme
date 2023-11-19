9eb47676d75a6f462ef396940b298a6719c9e7d1
Jeff Dairiki <dairiki@dairiki.org>
2023-11-18 21:41:33 -0800

    refactor: re-export the API bits from the top-level package

--- src/fancy_pypi_readme/__init__.py
+++ src/fancy_pypi_readme/__init__.py
@@ -1,3 +1,10 @@
 # SPDX-FileCopyrightText: 2022 Hynek Schlawack <hs@ox.cx>
 #
 # SPDX-License-Identifier: MIT
+
+from ._builder import build_text
+from ._config import load_and_validate_config
+from .exceptions import ConfigurationError
+
+
+__all__ = ["ConfigurationError", "build_text", "load_and_validate_config"]
